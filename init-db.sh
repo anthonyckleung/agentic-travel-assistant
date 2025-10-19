6a51f63c2984d55f462fff5f4d72ab56954f2764
anthonyckleung <anthonyleung124@gmail.com>
2025-10-19 22:19:48 +0000

    added langgraph setup

--- init-db.sh
+++ init-db.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-# Langgraph setup
 psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
+-- Setup for postgres user and database
 CREATE USER postgres WITH PASSWORD 'postgres_password';
 CREATE DATABASE postgresdb OWNER postgres;
 GRANT ALL PRIVILEGES ON DATABASE postgresdb TO postgres;
@@ -10,6 +10,14 @@ GRANT ALL PRIVILEGES ON DATABASE postgresdb TO postgres;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT CREATE ON SCHEMA public TO postgres;
 
-# Enable the pgvector extension in the database
+-- Enable the pgvector extension
 CREATE EXTENSION IF NOT EXISTS vector;
+
+-- Setup for langgraph user and database
+CREATE USER langgraph_user WITH PASSWORD 'langgraph_password';
+CREATE DATABASE langgraph_db OWNER langgraph_user;
+GRANT ALL PRIVILEGES ON DATABASE langgraph_db TO langgraph_user;
+\c langgraph_db
+GRANT ALL ON SCHEMA public TO langgraph_user;
+GRANT CREATE ON SCHEMA public TO langgraph_user;
 EOSQL
